9c494992f127439060c4bf4365a2364ae38ef6d5
Sazzad ur Rahman <22joylobo@gmail.com>
2022-03-16 08:49:46 +0600

    react app created

---
+++ src/note.js
@@ -0,0 +1,7 @@
+//todo 45-2 JSX, Dynamic content, Dynamic Style in React
+
+//JSX allows us to write HTML elements in JavaScript and place them in the DOM without any createElement() and/or appendChild() methods.
+
+//JSX is an XML/HTML-like syntax used by React that extends ECMAScript so that XML/HTML-like text can co-exist with JavaScript/React code.
+
+//JSX is a React extension to the JavaScript language syntax which provides a way to structure component rendering using syntax familiar to many developers.
